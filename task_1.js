5caa8458db92faa8a3ec7fe36b56e38be8d14427
Igor Vasin <vasin.igor0796@gmail.com>
2024-01-30 23:06:55 +0200

    first commit

---
+++ task_1.js
@@ -0,0 +1,7 @@
+
+let nameDevice = "Генератор";
+let price = 1000;
+console.log(`Обрано «${nameDevice}», ціна за одиницю ${price} кредитів`);
+
+price = 2000;
+console.log(`Обрано «${nameDevice}», ціна за одиницю ${price} кредитів`);
